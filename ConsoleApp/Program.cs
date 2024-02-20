5b92bf0f26d3ee25a142bf087bfa7e176ba37c2f
DurjoyAcharya <da-durjoy@outlook.com>
2024-02-20 11:01:42 +0600

    Modified

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,16 +13,45 @@ namespace ConsoleApp
             Console.WriteLine($"Age is {Age}");
         }
     }
-    
-    
+
+    internal class Student(string name, int age,string dept) :Person(name, age)
+    {
+        private string Dept { get; set; } = dept;
+
+        public override void Greet()
+        {
+            base.Greet();
+            Console.WriteLine($"Department is {this.Dept}");
+        }
+    }
+
+    internal class Shape
+    {
+        protected Shape(int size)
+        {
+            Size = size;
+            if (Size.Equals(3)) Console.WriteLine("This is Triangle Constructed ");
+            else if(Size.Equals(4)) Console.WriteLine("This is Square Constructed ");
+            else Console.WriteLine("Fu*king Constructed");
+        }
+
+        private int Size { get; set; }
+    }
+
+    internal class Triangle(int size) : Shape(size);
+
     abstract class Program
     {
-        
-        
         
         public static void Main(string[] args)
         {
             Console.WriteLine(new PlayWithKeys().GetClassInfo());
+            // var student = new Student("Durjoy Acharya", 25, "Software Engineering");
+            // student.Greet();
+            new Triangle(5);
+
+
+
         }
     }
 };
--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+640b80800a67740acc6f619add59b91a184edf92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a429e2a341f2e07110c3d8a98c4c561d30567787")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
