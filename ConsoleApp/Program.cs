a429e2a341f2e07110c3d8a98c4c561d30567787
DurjoyAcharya <da-durjoy@outlook.com>
2024-02-20 10:28:39 +0600

    added

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp.interview;
+
+namespace ConsoleApp
+{
+    internal class Person(string name,int age)
+    {
+        private string Name { get; set; } = name;
+        private int Age { get; set; } = age;
+
+        public virtual void Greet()
+        {
+            Console.WriteLine($"Name is {Name}");
+            Console.WriteLine($"Age is {Age}");
+        }
+    }
+    
+    
+    abstract class Program
+    {
+        
+        
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(new PlayWithKeys().GetClassInfo());
+        }
+    }
+};
+
