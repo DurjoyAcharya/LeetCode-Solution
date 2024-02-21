83ee88d8fa2facddf24af5f9a0c5543aaf855148
DurjoyAcharya <da-durjoy@outlook.com>
2024-02-21 17:50:38 +0600

    IsIsomorphic sol Added

--- ConsoleApp/CpSol/Solution.cs
+++ ConsoleApp/CpSol/Solution.cs
@@ -16,4 +16,39 @@ public class Solution
        }
        return 4;
     }
+
+    
+    //https://leetcode.com/problems/isomorphic-strings/
+    public bool IsIsomorphic(string s, string t) {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+        Dictionary<char, char> mapS = new Dictionary<char, char>();
+        Dictionary<char, char> mapT = new Dictionary<char, char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+
+            if (mapS.ContainsKey(s[i]))
+            {
+
+                if (mapS[s[i]] != t[i])
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (mapT.ContainsKey(t[i]))
+                {
+                    return false;
+                }
+                mapS.Add(s[i], t[i]);
+                mapT.Add(t[i], s[i]);
+            }
+        }
+
+        return true;
+    }
 }
