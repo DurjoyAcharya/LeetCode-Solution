abdad2322a7b640dbbaccbd6b1fb298f8db84b16
DurjoyAcharya <da-durjoy@outlook.com>
2024-02-23 17:33:55 +0600

    BookStore

---
+++ Models/WebBookSettings.cs
@@ -0,0 +1,10 @@
+namespace WebBooks.Models;
+
+public class WebBookSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string BooksCollectionName { get; set; } = null!;
+}
