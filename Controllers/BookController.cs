bd9af27a5bbca3f121c62239778345d8ca2a576c
DurjoyAcharya <da-durjoy@outlook.com>
2024-02-24 11:14:14 +0600

    done

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -5,7 +5,7 @@ using WebBooks.Services;
 namespace WebBooks.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/books")]
 public class BookController(WebBookService bookService) : ControllerBase
 {
     private readonly WebBookService _bookService = bookService;
@@ -15,5 +15,41 @@ public class BookController(WebBookService bookService) : ControllerBase
     public async Task<List<Book>> Get() =>
         await _bookService.GetAsync();
 
-  //  public async Task<string> Get() => await new Task<string>(() => "Hello World");
+    [HttpGet("{id:length(24)}")]
+    public async Task<ActionResult<Book>> Get(string id)
+    {
+        var book = await _bookService.GetSpecificAsync(id);
+        if (book==null)
+        {
+            return NotFound();
+        }
+        return book;
+    }
+    [HttpPost]
+    public async Task<IActionResult> Post(Book newBooks)
+    {
+        await _bookService.CreateAsync(newBooks);
+        return CreatedAtAction(nameof(Get), new { id = newBooks.Id },newBooks);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, Book newBooks)
+    {
+        var book = await _bookService.GetSpecificAsync(id);
+        if (book == null) return NotFound();
+        newBooks.Id = book.Id;
+        await _bookService.UpdateAsync(id, newBooks);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var book=await _bookService.GetSpecificAsync(id);
+        if (book == null) return NotFound();
+        await _bookService.RemoveOneAsync(id);
+        return NoContent();
+    }
+
+
 }
