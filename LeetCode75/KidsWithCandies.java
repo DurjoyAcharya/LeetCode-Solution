5930609e3191e464c7198dd2837a0052377f1376
DurjoyAcharya <da-durjoy@outlook.com>
2024-03-10 12:24:10 +0600

    candy Sol

---
+++ LeetCode75/KidsWithCandies.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithCandies {
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList<>();
+        int maxCandy=0;
+        for (int candy:candies){
+            maxCandy=Math.max(candy,maxCandy);
+        }
+
+        for (int i = 0; i < candies.length; i++)
+            result.add(candies[i]+extraCandies>=maxCandy);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        kidsWithCandies(new int[]{2,8,7},1).forEach(System.out::println);
+    }
+}
