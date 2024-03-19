f512f3c6756594be94ec5d2493ffac0412833edd
DurjoyAcharya <da-durjoy@outlook.com>
2024-03-20 01:33:12 +0600

    commit

--- Hack.py
+++ Hack.py
@@ -1,11 +1,22 @@
 import os
 from random import randint
 
-for i in range (1,365):
-    for j in range(0,randint(1,10)):
-        d= str(i)+' days ago'
-        with open('/home/syndicate/Music/LeetCode-Solution/hack.lua','a') as file:
-            file.write(d)
+# Loop through 1 to 364 (inclusive) for days
+for i in range(1, 365):
+    # Randomly select the number of commits for each day
+    for j in range(0, randint(1, 10)):
+        # Generate the date string for the commit
+        d = str(i) + ' days ago'
+        
+        # Open the file to append the commit information
+        with open('/home/syndicate/Music/LeetCode-Solution/hack.lua', 'a') as file:
+            file.write(d)  # Write the date
+        
+        # Stage changes to Git
         os.system('git add .')
-        os.system('git commit --date = " ' + d + ' " -m "commit"')
+        
+        # Commit with the specified date and a generic message
+        os.system('git commit --date="' + d + '" -m "commit"')
+
+# Push the changes to the remote repository
 os.system('git push -u origin main')
